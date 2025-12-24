81a0601a95234857c89410f1a6e0e7ccc2db595d
bewarb <pineapplepower04@gmail.com>
2025-12-24 02:06:04 -0500

    Intro to PyTorch Done

--- IntroToTorch.md
+++ IntroToTorch.md
@@ -20,4 +20,19 @@ Installing collected packages: mpmath, sympy, pillow, numpy, networkx, fsspec, f
 Successfully installed filelock-3.16.1 fsspec-2025.3.0 mpmath-1.3.0 networkx-3.1 numpy-1.24.4 pillow-10.4.0 sympy-1.13.3 torch-2.2.2 torchvision-0.17.2
 ```
 
-We are set to keep going.
+We are set to keep going. But, of course, new problem occurs. :D
+
+```
+ModuleNotFoundError: No module named 'torch'
+```
+
+Very annoying and I barely understand what exactly fixed, but either my python setup is extremely cooked. Or it just needed a moment to properly start working (two things can be true at once). Somehow, doing the install in a notebook caused both to start working...but we'll explore the exact reason when I factory reset this computer and never see OS X or windows ever again.
+
+Now we can move forward.
+
+## List of important functions and data types in Pytorch:
+
+#### Tensor
+```
+a = torch.ones(5,5)
+```
--- IntroToTorch.py
+++ IntroToTorch.py
@@ -4,4 +4,35 @@ import torch.nn as nn
 #This is a tensor
 
 a = torch.ones(5,5)
-print("Tensor a:")
+print(a)
+
+# Sum() and Mean() functions
+
+#Sum of the matrix
+sum1 = torch.sum(a)
+#Sum of each column
+sum2 = torch.sum(a, axis=1)
+#sum of each row
+sum3 = torch.sum(a, axis=0)
+
+print(sum3)
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.first_layer = nn.Linear(4,6) # Linear layer with 4 inputs and 6 outputs
+        self.second_layer = nn.Linear(6,6) # Linear layer with 6 inputs and 6 output
+        self.final_layer = nn.Linear(6,2) # Linear layer with 6 inputs and 2 output
+
+    def forward(self, x):
+        return self.final_layer(self.second_layer(self.first_layer(x)))
+    
+model = MyModel()
+
+input_data = torch.randn(1,4) # Random input tensor with batch size 3 and 4 features
+
+model_output = model(input_data)
+
+print(model_output)
+
+# This does not matter as this model is not trained yet.
